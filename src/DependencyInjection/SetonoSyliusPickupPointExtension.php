e8bf5a2d4dd7b16eb34f2dd59da62fc8d2836629
Joachim Løvgaard <joachim@loevgaard.dk>
2018-09-18 13:24:27 +0200

    removed boilerplate and renamed files/classes

--- src/DependencyInjection/SetonoSyliusPickupPointExtension.php
+++ src/DependencyInjection/SetonoSyliusPickupPointExtension.php
@@ -2,17 +2,19 @@
 
 declare(strict_types=1);
 
-namespace Acme\SyliusExamplePlugin\DependencyInjection;
+namespace Setono\SyliusPickupPointPlugin\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
-final class AcmeSyliusExampleExtension extends Extension
+final class SetonoSyliusPickupPointExtension extends Extension
 {
     /**
      * {@inheritdoc}
+     *
+     * @throws \Exception
      */
     public function load(array $config, ContainerBuilder $container): void
     {
--- src/SetonoSyliusPickupPointPlugin.php
+++ src/SetonoSyliusPickupPointPlugin.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-namespace Acme\SyliusExamplePlugin;
+namespace Setono\SyliusPickupPointPlugin;
 
 use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-final class AcmeSyliusExamplePlugin extends Bundle
+final class SetonoSyliusPickupPointPlugin extends Bundle
 {
     use SyliusPluginTrait;
 }
